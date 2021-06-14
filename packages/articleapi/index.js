43df7d9ac8896fb4d5758841f511eb9a23a6e566
fun <lljxx1@gmail.com>
2021-06-14 22:49:09 +0800

    feat(articleapi): support nodejs

--- packages/articleapi/index.js
+++ packages/articleapi/index.js
@@ -1,4 +1,23 @@
-// console.log('buildInDrivers', buildInDrivers)\
+const _bundle = require('./dist/code.js')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
-const libs = require('./dist/code.js')
-console.log(libs)
+async function publish(type, post) {
+    const _driver = _bundle.getDriver({
+      type: 'zhihu',
+    })
+
+    const _meta = await _driver.getMetaData()
+    console.log('_driver', _meta)
+}
+
+
+publish('zhihu', {
+  post_title: 'test',
+  post_content: 'content'
+})
+// console.log(
+//   bundle.getDriver({
+//     type: 'zhihu',
+//   })
+// )
