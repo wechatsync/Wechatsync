554acad160bc41bc65657af0c5e7cb94dd5e4900
--global <gmqiujun@gmail.com>
2023-11-02 19:30:01 +0800

    fix(web-extension): 修复右键提取文章同步公众号换行问题

--- packages/web-extension/src/viewer/App.vue
+++ packages/web-extension/src/viewer/App.vue
@@ -407,6 +407,7 @@ export default {
         post.desc = self.pageData.description
         // 优化微信公众号、知乎 html 中换行被处理为文章中换行问题
         post.content = post.content.replace(/\n+|\t+/g,"")
+        post.inline_content = post.inline_content.replace(/\n+|\t+/g,"")
 
         console.log("文章页抓取内容：" + post.content)
         return post
